3ac37d6d620a984e7df1acb929cd0212f1fcc72b
Abhay HM <abhayhmgi@gmail.com>
2021-05-03 15:17:41 +0530

    Main file prototype update

--- stats.c
+++ stats.c
@@ -27,13 +27,51 @@
 
 void main() {
 
-  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
-                              114, 88,   45,  76, 123,  87,  25,  23,
-                              200, 122, 150, 90,   92,  87, 177, 244,
-                              201,   6,  12,  60,   8,   2,   5,  67,
+	unsigned char buffer[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
+								114, 88,   45,  76, 123,  87,  25,  23,
+								200, 122, 150, 90,   92,  87, 177, 244,
+								201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90};
+	
+	printf("Analytics of an array of elements:\n");
+	print_statistics( &buffer, SIZE );
+	
+	printf("Reordered data set from large to small:\n");
+	sort_array( &buffer, SIZE );
+	
+}
 
-  /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
+void print_statistics( unsigned char *buffer, int nmemb )
+{
+	
+}
 
+void print_array( unsigned char *buffer, int nmemb )
+{
+	
+}
+
+unsigned char find_median( unsigned char *buffer, int nmemb )
+{
+	
+}
+
+unsigned char find_mean( unsigned char *buffer, int nmemb )
+{
+	
+}
+
+unsigned char find_maximum( unsigned char *buffer, int nmemb )
+{
+	
+}
+
+unsigned char find_minimum( unsigned char *buffer, int nmemb )
+{
+	
+}
+
+void sort_array( unsigned char *buffer, int nmemb )
+{
+	
 }
